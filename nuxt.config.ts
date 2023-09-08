682684c9e9914ab1ffaad346da4d77fc21221102
Jared Vargas <jaredwesley27@hotmail.com>
2023-09-08 01:31:42 -0500

    prueba

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  alias:{
-    assets:"/<rooDir>/public/assets"
-  },
   css:[
     // Load a Node.js module directly (here it's a Sass file)
     'bulma',
@@ -12,4 +9,18 @@ export default defineNuxtConfig({
      // SCSS file in the project
     '~/css/bulma/bulma.scss'
   ],
+  router: {
+    extendRoutes(routes) {
+      routes.forEach((route) => {
+        // When options.generate.subFolders is true (default)
+        const alias =
+          route.path.length > 1 ? `${route.path}/index.html` : '/index.html'
+        // When options.generate.subFolders is false
+        // const normalizedRoute = route.path.replace(/\/$/, '') // Remove trailing slashes if they exist
+        // const alias =
+        //   route.path.length > 1 ? `${normalizedRoute}.html` : '/index.html'
+        route.alias = alias
+      })
+    }
+  }
 })
