bb6adf70babf438ebfc113b81daf7ad1a004f84c
Jared Vargas <jaredwesley27@hotmail.com>
2023-09-08 01:40:22 -0500

    segundo intento sin router

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,18 +9,4 @@ export default defineNuxtConfig({
      // SCSS file in the project
     '~/css/bulma/bulma.scss'
   ],
-  router: {
-    extendRoutes(routes) {
-      routes.forEach((route) => {
-        // When options.generate.subFolders is true (default)
-        const alias =
-          route.path.length > 1 ? `${route.path}/index.html` : '/index.html'
-        // When options.generate.subFolders is false
-        // const normalizedRoute = route.path.replace(/\/$/, '') // Remove trailing slashes if they exist
-        // const alias =
-        //   route.path.length > 1 ? `${normalizedRoute}.html` : '/index.html'
-        route.alias = alias
-      })
-    }
-  }
 })
